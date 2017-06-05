b267ac0af5fcf4d16bcac15b7ae983583e041455
Lev Gogol <levgogol@gmail.com>
2017-06-05 20:32:28 +0300

    Реализован класс для работы со списком сообществ.

--- src/main/java/ru/scratty/action/util/VkGroup.java
+++ src/main/java/ru/scratty/action/util/VkGroup.java
@@ -3,7 +3,13 @@ package ru.scratty.action.util;
 import com.vk.api.sdk.client.actors.UserActor;
 import ru.scratty.action.common.VkApi;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Утилита для работы с группами vk
@@ -12,16 +18,60 @@ import java.util.ArrayList;
 
 public class VkGroup extends VkApi {
 
-    public VkGroup(UserActor userActor) {
+    private static final String PATH_GROUPS = "groups.list";
+
+    private static volatile VkGroup instance;
+
+    private final ArrayList<Integer> list = new ArrayList<>();
+
+    private VkGroup(UserActor userActor) {
         super(userActor);
+        if (checkExists()) {
+            init();
+        }
     }
 
-    public ArrayList<String> getListGroups() {
+    public static synchronized VkGroup getInstance(UserActor userActor) {
+        VkGroup vkGroup = instance;
+        if (vkGroup == null) {
+            synchronized (VkGroup.class) {
+                vkGroup = instance;
+                if (vkGroup == null) {
+                    instance = vkGroup = new VkGroup(userActor);
+                }
+            }
+        }
+        return vkGroup;
+    }
+
+    private void init() {
+        try {
+            Files.lines(Paths.get(PATH_GROUPS), StandardCharsets.UTF_8).forEach(s -> list.add(Integer.valueOf(s)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkExists() {
+        File file = new File(PATH_GROUPS);
+        if(!file.exists()) {
+            try {
+                file.getParentFile().mkdirs();
+                return file.createNewFile();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized Integer getRandomGroupId() {
+        return list.get(new Random().nextInt(list.size()));
+    }
+
+    public synchronized ArrayList<String> getListGroups() {
         return null;
-    }
-
-    public int getRandomGroupId() {
-
-        return 0;
     }
 }
